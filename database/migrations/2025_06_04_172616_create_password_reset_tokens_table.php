6138d508dea0a130e5ff573918a9566c7865d3ee
DarioMartinez11 <dariomvila@gmail.com>
2025-06-04 20:26:14 +0200

    5

--- database/migrations/2025_06_04_172616_create_password_reset_tokens_table.php
+++ database/migrations/2025_06_04_172616_create_password_reset_tokens_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email', 255);
             $table->string('token', 255);
-            $table->timestamps('created_at');
+            $table->timestamps();
 
             $table->primary('email');
         });
