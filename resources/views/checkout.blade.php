1a83b2dc2b782bc3f099929072aa0553bea10427
DarioMartinez11 <dariomvila@gmail.com>
2025-06-06 15:56:25 +0200

    si

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -2,6 +2,8 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>Finalizar compra | Pinturas General</title>
      <link rel="stylesheet" href="{{ asset('css/checkout/checkout.css') }}">
 
